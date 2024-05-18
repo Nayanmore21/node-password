012dd700a6f3a3c3ac72c0ef49d32faf0692fe38
Nayanmore21 <nayanmore2103@gmail.com>
2024-05-18 23:05:52 +0530

    Changed common js Module to Es module

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { program } from 'commander'
-const program=require('commander')
+// const program=require('commander')
 
 program.version('1.0.0').description('Simple Password Generator').parse()
