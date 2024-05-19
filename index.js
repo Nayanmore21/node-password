e81b1cbd8c621442a72bad7fea0811ffe1379fb4
Nayanmore21 <nayanmore2103@gmail.com>
2024-05-19 15:42:38 +0530

    chalk added

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import { program } from 'commander'
+import { Chalk } from 'chalk'
 const log=console.log
 import createPassword from './utils/createPassword.js'
 
 program.version('1.0.0').description('Simple Password Generator')
 
 program
-
 .option('-l,--length <number>','length of password','8')
 .option('-s,--save','save passwords to passwords.txt')
 .option('-nn,--no-numbers','remove numbers')
@@ -19,4 +19,4 @@ const {length,save,numbers,symbols}=program.opts()
 const generatedPassword=createPassword(length,numbers,symbols)
 
 //output generated
-log(generatedPassword)
+log(Chalk.blue('Generated password:') + Chalk.bold(generatedPassword))
